989c6f8ddffd2d442fa408cea87f0221611da24f
Basudev Nayak <51286951+basudevnayak@users.noreply.github.com>
2025-04-21 23:29:32 +0530

    add

--- api/index.js
+++ api/index.js
@@ -1,41 +1,13 @@
-import express from 'express';
-import serverless from 'serverless-http';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import { authRoutes, VariablesRoutes } from '../routes/index.js';
-import errorHandler from '../middlewares/errorHandler.js';
-import CustomErrorHandler from '../utils/CustomErrorHandler.js';
-import { connect } from '../config/connect.js';
-import { DATABASE_URL } from '../config/index.js';
+// api/index.js
+import express from 'express'
+import serverless from 'serverless-http'
 
-dotenv.config();
+const app = express()
 
-const app = express();
+app.use(express.json())
 
-// Middlewares
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.get('/hello', (req, res) => {
+  res.json({ message: 'Hello from Serverless Express on Vercel' })
+})
 
-// Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/variables', VariablesRoutes);
-
-app.get('/', (req, res) => {
-  res.send('<h1>Welcome to E-commerce API on Vercel</h1>');
-});
-
-app.use(errorHandler);
-
-// Connect DB (optional, make sure this doesn't reconnect on every call)
-let isConnected = false;
-app.use(async (req, res, next) => {
-  if (!isConnected) {
-    await connect(DATABASE_URL);
-    isConnected = true;
-  }
-  next();
-});
-
-// Export as serverless function
-export default serverless(app);
+export const handler = serverless(app)
